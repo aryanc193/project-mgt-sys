83258dd4e8b7f23c37edac7fe0c788d3a47f3e04
Aryan <aryanc193@gmail.com>
2025-12-16 03:15:12 +0530

    chore: postgres and graphql setup

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "graphene_django",
     "core",
 ]
 
@@ -74,11 +75,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "project_management",
+        "USER": "postgres",
+        "PASSWORD": "mosshead123",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
+
 
 
 # Password validation
@@ -116,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+GRAPHENE = {
+    "SCHEMA": "core.schema.schema",
+}
